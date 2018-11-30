aa60aaf36842c1fd43da3ed080927a837b3b6011
Asia Carter <asiamcarter@gmail.com>
2018-11-29 18:29:10 -0600

    added all functions to all eventHandlers

--- powers.js
+++ powers.js
@@ -5,5 +5,21 @@ function flightHandlerFunction () {
     flightSection.classList.replace("disabled", "enabled");
 }
 
-//click event listenor on "activate flight" button 
+function mindHandlerFunction () {
+    var mindreadingSection = document.getElementById("mindreading");
+    mindreadingSection.classList.replace("disabled", "enabled");
+}
+
+function xrayHandlerFunction () {
+    var xraySection = document.getElementById("xray");
+    xraySection.classList.replace("disabled", "enabled");
+}
+//click event listener on "activate flight" button 
 document.querySelector("#activate-flight").addEventListener("click", flightHandlerFunction);
+
+//click event listener on "mindreading" button
+document.querySelector("#activate-mindreading").addEventListener("click", mindHandlerFunction);
+
+//click event listener on "X-ray Vision" button
+document.querySelector("#activate-xray").addEventListener("click", xrayHandlerFunction);
+
